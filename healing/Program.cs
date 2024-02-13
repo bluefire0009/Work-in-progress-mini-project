a9f36a96c1b0e56af086396f87011d00275893fc
BoraHR <159787942+BoraHR@users.noreply.github.com>
2024-02-13 11:01:49 +0100

    Healing mechanic
    NOTE: view "/healing/Program.cs" rest are extations of compiler.
    
    includes:
    - if statements with item id thats used in parameter.
    - HP thats added to Player.HP atribiute.
    - Text of added HP info.

---
+++ healing/Program.cs
@@ -0,0 +1,30 @@
+﻿public static class Healing
+{
+    public static void heal(int Heal.id)
+    {
+        if (heal.id == 1)
+            Player.HP += 20
+            Console.Write($"Healed 20HP")
+
+        else if (heal.id == 2)
+            Player.HP += 50
+            Console.Write($"Healed 50HP")
+
+        else if (heal.id == 3)
+            Player.HP += 80
+            Console.Write($"Healed 80HP")
+
+        else if (heal.id == 4)
+            Player.HP += 150
+            Console.Write($"Healed 150HP")
+
+        else if (heal.id == 4)
+            Player.HP += 300
+            Console.Write($"Healed 300HP")
+
+        else  // fail save
+            Player.HP += 0
+            Console.Write($"Healed <NULL>HP")
+
+    }
+}
