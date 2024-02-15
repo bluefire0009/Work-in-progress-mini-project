914afd01f47c01961534597a5bcef4e2a906d5b7
bluefire0009 <1032628@hr.nl>
2024-02-15 13:31:54 +0100

    Small update, no new features added

--- Work in progress/Location.cs
+++ Work in progress/Location.cs
@@ -3,12 +3,12 @@ class Location
     public int ID;
     public string Name;
     public string Description;
-    public Quest? QuestAvailableHere;
-    public Monster? MonsterLivingHere;
-    public Location? LocationToNorth;
-    public Location? LocationToEast;
-    public Location? LocationToSouth;
-    public Location? LocationToWest;
+    public Quest QuestAvailableHere;
+    public Monster MonsterLivingHere;
+    public Location LocationToNorth;
+    public Location LocationToEast;
+    public Location LocationToSouth;
+    public Location LocationToWest;
 
     public Location(int id, string name, string description, Quest? questHere = null, Monster? monsterHere = null, 
                     Location? locationN = null, Location? locationE = null, Location? locationS = null, Location? locationW = null)
@@ -23,13 +23,12 @@ class Location
         LocationToSouth = locationS;
         LocationToWest = locationW;
     }
+
+
+
     public void SetNorthTo(Location location) => LocationToNorth = location;
     public void SetEastTo(Location location) => LocationToEast = location;
     public void SetSouthTo(Location location) => LocationToSouth = location;
     public void SetWestTo(Location location) => LocationToWest = location;
 
-    public string Compass()
-    {
-        return"";
-    }
 }
