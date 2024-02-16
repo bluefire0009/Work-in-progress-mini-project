dd6a0ea0bc928fa402b83585504bb58f1ff0d685
Motje <Mohamaadflaha2014@gmail.com>
2024-02-16 10:00:53 +0100

    updating player class, and made new world file which will be used later to intilaize the world

--- Work in progress/obj/Debug/net7.0/Work in progress.AssemblyInfo.cs
+++ Work in progress/obj/Debug/net7.0/Work in progress.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Work in progress")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb3b7853adcd67b70d8dfb5c67f0b7a6893f7e68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4752e8d9c64535c0de451c79e7b0599469d7fe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Work in progress")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Work in progress")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
