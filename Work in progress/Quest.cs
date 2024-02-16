42f66381ed607bf72e6552ad09e81c69cb789371
Motje <Mohamaadflaha2014@gmail.com>
2024-02-16 09:38:55 +0100

    Updated quest and added new world file

--- Work in progress/Quest.cs
+++ Work in progress/Quest.cs
@@ -1,4 +1,19 @@
 public class Quest
 {
-    
+    public int ID { get; }
+    public string Name { get; }
+    public string Description { get; }
+    public int GoalAmount { get; }
+    public int CurrentAmount { get; private set; }
+    public bool IsCompleted => CurrentAmount >= GoalAmount;
+
+    public Quest(int id, string name, string description, int goalAmount)
+    {
+        ID = id;
+        Name = name;
+        Description = description;
+        GoalAmount = goalAmount;
+        CurrentAmount = 0;
+    }
+
 }
