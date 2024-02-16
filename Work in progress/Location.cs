3d6a3c2fb066dc6da0413719a4d7f55e00042dca
Motje <Mohamaadflaha2014@gmail.com>
2024-02-16 10:23:29 +0100

    fixed world class

--- Work in progress/Location.cs
+++ Work in progress/Location.cs
@@ -1,22 +1,25 @@
 public class Location
 {
-    public int Id { get; private set; }
+    public int ID { get; private set; }
     public string Name { get; private set; }
     public string Description { get; private set; }
     public bool IsCompleted { get; private set; }
     public bool IsAccessible { get; private set; }
     public Quest QuestAvailableHere { get; set; }
     public Monster MonsterLivingHere { get; set; }
+    public Location LocationToNorth { get; set; }
+    public Location LocationToSouth { get; set; }
+    public Location LocationToEast { get; set; }
+    public Location LocationToWest { get; set; }
 
     public Location(int id, string name, string description, bool isCompleted, bool isAccessible)
     {
-        Id = id;
+        ID = id;
         Name = name;
         Description = description;
         IsCompleted = isCompleted;
         IsAccessible = isAccessible;
     }
-
-    // Additional methods and logic can be added here
+  // Additional methods and logic can be added here
     // For example, methods to update IsCompleted and IsAccessible statuses
 }
