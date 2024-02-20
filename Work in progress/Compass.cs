46d27907cf37cacc03d7af9cb122c104aa2b4446
bluefire0009 <1032628@hr.nl>
2024-02-20 21:05:35 +0000

    Add showMap method which prints the map in a human readable way, doesnt work completely yet, because the offset is not calculated correctly yet

--- Work in progress/Compass.cs
+++ Work in progress/Compass.cs
@@ -2,12 +2,12 @@ class Compass
 {
     public List<string> locationNames = new();
     List<Location> Locations;
-    public string Nname;
-    public string Wname;
-    public string Ename;
-    public string Sname;
+    private string Nname;
+    private string Wname;
+    private string Ename;
+    private string Sname;
     public List<List<string>> Map;
-
+    
 
 
     public Compass(List<Location> locations)
@@ -18,10 +18,10 @@ class Compass
             locationNames.Add(location.Name);
         }
         Map = makeMap();
-        Console.WriteLine();
     }
 
-    public List<List<string>> makeMap()
+    // Makes a map based upon locations given in the constructor
+    protected List<List<string>> makeMap()
     {
         
         List<List<string>> map = new List<List<string>>();
@@ -36,7 +36,7 @@ class Compass
             map.Add(emptyList);
         }
         // Add first location (home) to the map
-        map[10][10] = "Home";
+        map[10][2] = "Home";
         // Go through all the locations in the initialized Locations list
         foreach (Location location in Locations)
         {
@@ -48,17 +48,16 @@ class Compass
             if (location.LocationToNorth != null)
                 Nname = location.LocationToNorth.Name;
             if (location.LocationToWest != null) {
-                var Test = location.LocationToWest.Name != null;
                 Wname = location.LocationToWest.Name; }
             if (location.LocationToEast != null)
                 Ename = location.LocationToEast.Name;
             if (location.LocationToSouth != null)
                 Sname = location.LocationToSouth.Name;
             // Go through all the locations currently in the map
-            for(int i0 = 0; i0<map.Count; i0++)
+            for(int i0 = 0; i0<map.Count; i0+=2)
             {
                 List<string>row = map[i0];
-                for (int i1 = 0; i1<row.Count; i1++)
+                for (int i1 = 0; i1<row.Count; i1+=2)
                 {
                     string column = row[i1];
                     // if the current location name is the same as the locations in the row, add all the locations to the sides to the map
@@ -66,45 +65,92 @@ class Compass
                     {
                         map[i0][i1] = locName;
                         if (Nname != "")
-                            map[i0][i1 + 1] = Nname;
+                            map[i0][i1 + 2] = Nname;
                         if (Wname != "")
-                            map[i0-1][i1] = Wname;
+                            map[i0 - 2][i1] = Wname;
                         if (Ename != "")
-                            map[i0 + 1][i1] = Ename;
+                            map[i0 + 2][i1] = Ename;
                         if (Sname != "")
-                            map[i0][i1 - 1] = Sname;
+                            map[i0][i1 - 2] = Sname;
                     }
                 }
             }
         }
         // Add allowed directions to the location string
-        for (int i0=0; i0<map.Count; i0++)
+        for (int i0=0; i0<map.Count; i0+=2)
         {
             List<string> row = map[i0];
-            for(int i1=0; i1<row.Count; i1++)
+            for(int i1=0; i1<row.Count; i1+=2)
             {
                 string column = row[i1];
-                if (locationNames.Contains(column))
+                if (locationNames.Contains(column) == false)
                 {
-                    foreach(Location location in Locations)
+                    continue;
+                }
+                foreach(Location location in Locations)
+                {
+                    if (location.Name == column) 
                     {
-                        if (location.Name == column) 
-                        {
-                            if (location.LocationToNorth != null)
-                                map[i0][i1] += ",North";
-                            if (location.LocationToWest != null)
-                                map[i0][i1] += ",West";
-                            if (location.LocationToEast != null)
-                                map[i0][i1] += ",East";
-                            if (location.LocationToSouth != null)
-                                map[i0][i1] += ",South";
+                        // add offset
+                        //TO DO: make this into a function, make offset derive from something static, i.e. name length
+                        map[i0][i1] = $"{stringTimes(i1," ")},{map[i0][i1]}";
+                        if (location.LocationToNorth != null)
+                        {    
+                            map[i0][i1] += ",North";
+                            map[i0][i1+1] ="--";
                         }
-
+                        if (location.LocationToWest != null)
+                        {
+                            map[i0][i1] += ",West";
+                            map[i0-1][i1] =$"{stringTimes(i1+(location.LocationToWest.Name.Length/2)," ")}|";
+                        }
+                        if (location.LocationToEast != null)
+                        {
+                            map[i0][i1] += ",East";
+                            map[i0+1][i1] =$"{stringTimes(i1 + (location.LocationToEast.Name.Length/2)," ")}|";
+                        }
+                        if (location.LocationToSouth != null)
+                        {
+                            map[i0][i1] += ",South";
+                            map[i0][i1-1] ="--";
+                        }
                     }
+
                 }
             }
         }
         return map;
-    } 
-        
+    }
+
+    public void showMap()
+    {
+        for(int i0 = 0; i0<Map.Count; i0++)
+        {
+            var row = Map[i0];
+            for(int i1 = 0; i1<row.Count; i1++)
+            {
+                var column = row[i1];
+                if (column=="")
+                    Console.Write($" ");
+                else
+                    if (column.Contains('|') | column.Contains("--"))
+                        Console.Write($"{Map[i0][i1]}");
+                    else if (Map[i0][i1-1] == "")
+                        Console.Write($"{Map[i0][i1].Split(",")[0]}{Map[i0][i1].Split(",")[1]}");
+                    else
+                        Console.Write($"{Map[i0][i1].Split(",")[1]}");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    private string stringTimes(int times, string toMultiply)
+    {
+        string ret = "";
+        for (int i0 = 0; i0< times; i0++)
+        {
+            ret += toMultiply;
+        }
+        return ret;
+    }
 }
--- Work in progress/compass_testing.cs
+++ Work in progress/compass_testing.cs
@@ -1,16 +1,17 @@
 public class testing_compass {
     public void test()
     {
-        List<Location> L = new List<Location>();
-        int LOCATION_ID_HOME = 1;
-        int LOCATION_ID_TOWN_SQUARE = 2;
-        int LOCATION_ID_GUARD_POST = 3;
-        int LOCATION_ID_ALCHEMIST_HUT = 4;
-        int LOCATION_ID_ALCHEMISTS_GARDEN = 5;
-        int LOCATION_ID_FARMHOUSE = 6;
-        int LOCATION_ID_FARM_FIELD = 7;
-        int LOCATION_ID_BRIDGE = 8;
-        int LOCATION_ID_SPIDER_FIELD = 9;
+        List<Location> Locations = new List<Location>();
+        const int LOCATION_ID_HOME = 1;
+        const int LOCATION_ID_TOWN_SQUARE = 2;
+        const int LOCATION_ID_GUARD_POST = 3;
+        const int LOCATION_ID_ALCHEMIST_HUT = 4;
+        const int LOCATION_ID_ALCHEMISTS_GARDEN = 5;
+        const int LOCATION_ID_FARMHOUSE = 6;
+        const int LOCATION_ID_FARM_FIELD = 7;
+        const int LOCATION_ID_BRIDGE = 8;
+        const int LOCATION_ID_SPIDER_FIELD = 9;
+        
         Location home = new Location(LOCATION_ID_HOME, "Home", "Your house. You really need to clean up the place.", null, null);
         Location townSquare = new Location(LOCATION_ID_TOWN_SQUARE, "Town square", "You see a fountain.", null, null);
         Location alchemistHut = new Location(LOCATION_ID_ALCHEMIST_HUT, "Alchemist's hut", "There are many strange plants on the shelves.", null, null);
@@ -20,6 +21,7 @@ public class testing_compass {
         Location guardPost = new Location(LOCATION_ID_GUARD_POST, "Guard post", "There is a large, tough-looking guard here.", null, null);
         Location bridge = new Location(LOCATION_ID_BRIDGE, "Bridge", "A stone bridge crosses a wide river.", null, null);
         Location spiderField = new Location(LOCATION_ID_SPIDER_FIELD, "Forest", "You see spider webs covering covering the trees in this forest.", null, null);
+        Location myTest = new Location(999,"TESTEST","ABCDEF",null,null);
         // Link the locations together
         home.LocationToNorth = townSquare;
 
@@ -37,6 +39,7 @@ public class testing_compass {
         alchemistHut.LocationToNorth = alchemistsGarden;
 
         alchemistsGarden.LocationToSouth = alchemistHut;
+        alchemistsGarden.LocationToWest = myTest;
 
         guardPost.LocationToEast = bridge;
         guardPost.LocationToWest = townSquare;
@@ -46,17 +49,20 @@ public class testing_compass {
 
         spiderField.LocationToWest = bridge;
 
+        myTest.LocationToEast = alchemistsGarden;
+
         // Add the locations to the static list
-        L.Add(home);
-        L.Add(townSquare);
-        L.Add(guardPost);
-        L.Add(alchemistHut);
-        L.Add(alchemistsGarden);
-        L.Add(farmhouse);
-        L.Add(farmersField);
-        L.Add(bridge);
-        L.Add(spiderField);
-        Compass C = new(L);
-        C.makeMap();
+        Locations.Add(home);
+        Locations.Add(townSquare);
+        Locations.Add(guardPost);
+        Locations.Add(alchemistHut);
+        Locations.Add(alchemistsGarden);
+        Locations.Add(farmhouse);
+        Locations.Add(farmersField);
+        Locations.Add(bridge);
+        Locations.Add(spiderField);
+        Locations.Add(myTest);
+        Compass compass = new(Locations);
+        compass.showMap();
     }
 }
